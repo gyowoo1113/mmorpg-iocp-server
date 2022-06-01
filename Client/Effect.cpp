5f331461ecd3d5b52fe13e05f18535724b4158c7
gyowoo1113 <yereube@naver.com>
2022-06-01 16:31:21 +0900

    Refactor : effect class  멤버함수 선언 정의 분리 및 정리

--- Client/Effect.cpp
+++ Client/Effect.cpp
@@ -1,2 +1,35 @@
 #include "stdafx.h"
 #include "Effect.h"
+
+CEffect::CEffect(sf::Texture& t, int x, int y, int x2, int y2, int frameCount)
+	: CGameObject(t, x, y, x2, y2)
+{
+	setFrameCount(frameCount);
+}
+
+void CEffect::animDraw()
+{
+	if (false == m_bActive) return;
+
+	if (m_nIdleIndex >= m_nFrameCount)
+		initIndex();
+
+	m_sprite.setTextureRect(sf::IntRect(m_nIdleIndex * 16, 0, 16, 16));
+
+	CGameObject::draw();
+
+	++m_nIdleIndex;
+}
+
+void CEffect::draw()
+{
+	if (false == m_bActive) return;
+
+	m_sprite.setTextureRect(sf::IntRect(m_nIdleIndex * 16, 0, 16, 16));
+	CGameObject::draw();
+}
+
+bool CEffect::isEndFrame()
+{
+	return (m_nIdleIndex >= m_nFrameCount);
+}
--- Client/Effect.h
+++ Client/Effect.h
@@ -5,48 +5,12 @@
 class CEffect : public CGameObject
 {
 public:
-	CEffect(sf::Texture& t, int x, int y, int x2, int y2, int frameCount) : CGameObject(t, x, y, x2, y2)
-	{
-		setFrameCount(frameCount);
-	}
-	CEffect()
-	{
+	CEffect(sf::Texture& t, int x, int y, int x2, int y2, int frameCount);
+	CEffect() = default;
 
-	}
-
-	virtual void animDraw() {
-		if (false == m_bActive) return;
-
-		if (m_nIdleIndex >= m_nFrameCount)
-			initEffect();
-
-		m_sprite.setTextureRect(sf::IntRect(m_nIdleIndex * 16, 0 , 16, 16));
-
-		CGameObject::draw();
-
-		++m_nIdleIndex;
-	}
-
-	virtual void draw() {
-		if (false == m_bActive) return;
-
-		m_sprite.setTextureRect(sf::IntRect(m_nIdleIndex * 16, 0, 16, 16));
-		CGameObject::draw();
-	}
-
-	void updateIndex() {
-		++m_nIdleIndex;
-	}
-
-	void initEffect()
-	{
-		m_nIdleIndex = 0;
-		//m_active = false;
-	}
-
-	bool isEndFrame()
-	{
-		return (m_nIdleIndex >= m_nFrameCount);
-	}
+	virtual void animDraw() override;
+	virtual void draw() override;
+	
+	bool isEndFrame();
 };
 
